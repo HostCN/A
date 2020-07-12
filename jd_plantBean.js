d76d8b72fefefac8ebcc7a4e4b44216656857955
Cloud <50728970+HostCN@users.noreply.github.com>
2020-07-12 14:38:49 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -97,7 +97,8 @@ const name = '京东种豆得豆'
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 var plantUuids = [ // 这个列表填入你要助力的好友的plantUuid
     'tdwxwgkotfye2aug2vimwzpy5s4dy6tosh5qmka',
-    'ogbxzlsffvjbqhx5xsrcdncige5ac3f4ijdgqji'
+    'ogbxzlsffvjbqhx5xsrcdncige5ac3f4ijdgqji',
+    'jezqczgr4tseckfq2b2pf3lwl',
 ]
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
