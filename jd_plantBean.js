14179c5f86103164f0409c62cc1b78180ea1a22d
Cloud <50728970+HostCN@users.noreply.github.com>
2020-07-12 14:44:25 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -98,7 +98,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 var plantUuids = [ // 这个列表填入你要助力的好友的plantUuid
     'tdwxwgkotfye2aug2vimwzpy5s4dy6tosh5qmka',
     'ogbxzlsffvjbqhx5xsrcdncige5ac3f4ijdgqji',
-    'jezqczgr4tseckfq2b2pf3lwl',
+    'jezqczgr4tseckfq2b2pf3lwl4',
 ]
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
